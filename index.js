9823b9eed69279359ea6182b7a0d2e7a1b08978e
mambop <pbiscolt@gmal.com>
2021-07-07 20:47:48 +0300

    CORS fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const orderRoutes = require("./routers/orderRoutes");
 const adminRoutes = require("./routers/adminRoutes");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
+// const cors = require("cors");
 
 dotenv.config();
 
@@ -18,30 +18,69 @@ app.use(express.json());
 app.use(cookieParser());
 
 // allow client make req,res & save cookie 
-app.use(cors({
-  origin:["https://safe-courier-phillip.netlify.app","http://localhost:3000"],
-  credentials:true
-}));
+// app.use(cors({
+//   origin:["https://safe-courier-phillip.netlify.app","http://localhost:3000"],
+//   credentials:true
+// }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://safe-courier-phillip.netlify.app');
+  res.header(
+    'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization,  X-PINGOTHER'
+  );
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
+  next();
+});
+
+// const origin = process.env.NODE_ENV === "development" 
+//   ? "http://localhost:3000" 
+//   : "http://example.com"
+
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin
+//   }),
 
 
+// connect to mongoDB
+function establishedConnectin(){
+  console.log("\nEstablishing Database Connection . . . ");
+  mongoose.connect(
+    process.env.DB_URL_DEV,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+  
+    }).then(() => {
+      console.info("\nDatabase Connection Established!");
+    }).catch((err) => {
+      console.log("\nDatabase Connection Failed!");
+      console.error("Error Details: ", err);
+      console.log("\n\nDatabase Connection Failed, Retrying . . .");
+      establishedConnectin();
+    });
+  }
+   
+    establishedConnectin();
+// mongoose.connect(
+//   process.env.DB_URL_LOC,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
 
-//connect to mongoDB
-mongoose.connect(
-  process.env.DB_URL_DEV,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-
-  }).then(() => {
-    console.info("\nDatabase Connection Established!");
-  }).catch((err) => {
-    console.log("\nDatabase Connection Failed!");
-    console.error("Error Details: ", err);
-    console.log("\n\nDatabase Connection Failed, Retrying . . .");
-  })
+//   }).then(() => {
+//     console.info("\nDatabase Connection Established!");
+//   }).catch((err) => {
+//     console.log("\nDatabase Connection Failed!");
+//     console.error("Error Details: ", err);
+//     console.log("\n\nDatabase Connection Failed, Retrying . . .");
+//   })
 
 //set up routes
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/users", orderRoutes);
 app.use("/api/v1/admin", adminRoutes);
 app.use("/api/v1/orders", orderRoutes);
+
